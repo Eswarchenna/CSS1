4eb16ae69a11fe1796b24612d908b7eee29cb29f
Eswarchenna <chennaeshwar7@gmail.com>
2026-06-13 13:02:03 +0530

    Add files via upload

---
+++ ColourChange.js
@@ -0,0 +1,11 @@
+function onClickChangeColour() {
+    debugger;
+    // document.getElementById("divResult");
+    var divResult = document.getElementById("divResult");
+    divResult.style.backgroundColor = "lightgreen";
+    divResult.style.borderColor = "#312b2b";
+    divResult.style.borderRadius = "10px";
+    divResult.style.borderStyle = "dotted";
+    divResult.style.padding = "50px";
+    divResult.style.width = "345px";
+}
